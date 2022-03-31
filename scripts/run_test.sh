6885c421e6c99f6e3c2ed4b32ae657115263d26b
ayeffkay <chatton1@mail.ru>
2022-03-31 13:01:45 +0300

    refactor: scripts update

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -2,6 +2,6 @@
 
 python dialog_runner.py \
         --plots_folder dialog_plots \
-        --start_label greeting_flow start_node \
-        --fallback_label greeting_flow node_goodbye \
-        --dialog_test_file tests/test2.in
+        --start_label global_flow start_node \
+        --fallback_label global_flow node_goodbye \
+        --dialog_test_file tests/test3.in
