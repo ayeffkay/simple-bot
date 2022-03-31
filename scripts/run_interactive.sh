bc1b46201a194f48d636ece293b868a92e5cbcd9
ayeffkay <chatton1@mail.ru>
2022-03-31 13:08:13 +0300

    refactor: nodes in interactive mode

--- scripts/run_interactive.sh
+++ scripts/run_interactive.sh
@@ -2,6 +2,6 @@
 
 python dialog_runner.py \
         --plots_folder dialog_plots \
-        --start_label how_are_you_flow start_node \
-        --fallback_label how_are_you_flow fallback_node \
+        --start_label global_flow start_node \
+        --fallback_label global_flow node_goodbye \
         --run_interactive
